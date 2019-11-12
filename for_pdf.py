07b4d7880f2e4c5c7a45ef5127834a13dc3a3d27
abhijeet0810 <abhijeet@lore.ai>
2019-11-12 14:52:01 +0100

    some changes

--- for_pdf.py
+++ for_pdf.py
@@ -28,6 +28,27 @@ data = [['Abv', 45, 23, 52.00],
 
 make_table(data=data)
 
+def make_paragraph(paragraph):
+
+        epw = pdf.w - 2*pdf.l_margin
+
+        pdf.multi_cell(epw, 0.15, paragraph, border=1)
+        pdf.ln(0.5)
+
+dummy_text= """Lorem ipsum dolor sit amet, vel ne quando dissentias. Ne his opo\
+rteat expetendis. Ei tantas explicari quo, sea vidit minimum menandri ea. His ca\
+se errem dicam ex, mel eruditi tibique delicatissimi ut. At mea wisi dolorum con\
+tentiones, in malis vitae viderer mel.
+Vis at dolores ocurreret splendide. Noster dolorum repudiare vis ei, te augue su\
+mmo vis. An vim quas torquatos, electram posidonium eam ea, eros blandit ea vel.\
+Reque summo assueverit an sit. Sed nibh conceptam cu, pro in graeci ancillae co\
+nstituto, eam eu oratio soleat instructior. No deleniti quaerendum vim, assum sa\
+epe munere ea vis, te tale tempor sit. An sed debet ocurreret adversarium, ne en\
+im docendi mandamus sea.
+"""
+
+make_paragraph(paragraph= dummy_text)
+
 pdf.output('simple_inv.pdf','F')
 
 
